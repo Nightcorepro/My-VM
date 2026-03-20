a61caa54427a7b12bfcc408845875687c6bb891c
Nightcorepro <abhijeetmandal2050@gmail.com>
2026-03-20 17:05:55 +0530

    Remove initial Git setup commands from Hi.js

--- Hi.node.js
+++
@@ -1,9 +0,0 @@
-git@github.com:Nightcorepro/My-VM.git
-
-echo "# My-VM" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Nightcorepro/My-VM.git
-git push -u origin main
